df0da9aae5b0e4439e797e95ed89acb8d07b8e41
Carlos M. Perez <carlosm.perez@gmail.com>
2020-04-02 18:03:45 +0200

    File treeview done

--- MWPManager/Main.Designer.cs
+++ MWPManager/Main.Designer.cs
@@ -28,38 +28,73 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
-            this.listView1 = new System.Windows.Forms.ListView();
+            this.lstFiles = new System.Windows.Forms.ListView();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.tvFolders = new System.Windows.Forms.TreeView();
+            this.imgIcons = new System.Windows.Forms.ImageList(this.components);
+            this.colHeader = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // listView1
+            // lstFiles
             // 
-            this.listView1.HideSelection = false;
-            this.listView1.Location = new System.Drawing.Point(818, 126);
-            this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(439, 444);
-            this.listView1.TabIndex = 0;
-            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.lstFiles.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.colHeader});
+            this.lstFiles.FullRowSelect = true;
+            this.lstFiles.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.None;
+            this.lstFiles.HideSelection = false;
+            this.lstFiles.Location = new System.Drawing.Point(12, 327);
+            this.lstFiles.Name = "lstFiles";
+            this.lstFiles.Size = new System.Drawing.Size(439, 243);
+            this.lstFiles.SmallImageList = this.imgIcons;
+            this.lstFiles.TabIndex = 0;
+            this.lstFiles.UseCompatibleStateImageBehavior = false;
+            this.lstFiles.View = System.Windows.Forms.View.Details;
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Location = new System.Drawing.Point(457, 12);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(315, 558);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
             // 
+            // tvFolders
+            // 
+            this.tvFolders.ImageIndex = 0;
+            this.tvFolders.ImageList = this.imgIcons;
+            this.tvFolders.Location = new System.Drawing.Point(12, 12);
+            this.tvFolders.Name = "tvFolders";
+            this.tvFolders.SelectedImageIndex = 0;
+            this.tvFolders.Size = new System.Drawing.Size(439, 309);
+            this.tvFolders.TabIndex = 2;
+            this.tvFolders.NodeMouseClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.tvFolders_NodeMouseClick);
+            // 
+            // imgIcons
+            // 
+            this.imgIcons.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imgIcons.ImageStream")));
+            this.imgIcons.TransparentColor = System.Drawing.Color.Transparent;
+            this.imgIcons.Images.SetKeyName(0, "HardDrive");
+            this.imgIcons.Images.SetKeyName(1, "FolderClosed");
+            this.imgIcons.Images.SetKeyName(2, "FolderOpened");
+            this.imgIcons.Images.SetKeyName(3, "ImageFile");
+            // 
+            // colHeader
+            // 
+            this.colHeader.Width = 100;
+            // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1269, 582);
+            this.ClientSize = new System.Drawing.Size(896, 582);
+            this.Controls.Add(this.tvFolders);
             this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.lstFiles);
             this.Font = new System.Drawing.Font("Verdana", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.Margin = new System.Windows.Forms.Padding(4);
@@ -67,6 +102,7 @@
             this.Name = "Main";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Mobile Wallpaper Manager";
+            this.Load += new System.EventHandler(this.Main_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
@@ -74,8 +110,11 @@
 
         #endregion
 
-        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ListView lstFiles;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TreeView tvFolders;
+        private System.Windows.Forms.ImageList imgIcons;
+        private System.Windows.Forms.ColumnHeader colHeader;
     }
 }
 
--- MWPManager/Main.cs
+++ MWPManager/Main.cs
@@ -1,20 +1,134 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Windows.Forms;
 
 namespace MWPManager
 {
     public partial class Main : Form
     {
+        public enum Icons
+        {
+            HardDrive = 0,
+            FolderClosed = 1,
+            FolderOpen = 2,
+            ImageFile = 3
+        }
+
         public Main()
         {
             InitializeComponent();
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            PopulateTreeView();
+        }
+
+        private void PopulateTreeView()
+        {
+            TreeNode rootNode;
+
+            DriveInfo[] drives = DriveInfo.GetDrives();
+
+            foreach(DriveInfo drive in drives)
+            {
+                rootNode = new TreeNode(drive.Name, (int)Icons.HardDrive, (int)Icons.HardDrive);
+                rootNode.Tag = drive;
+                tvFolders.Nodes.Add(rootNode);
+                GetFolders(drive.RootDirectory.GetDirectories(), rootNode);
+                GetFiles(drive);
+            }
+        }
+
+        private void GetFolders(DirectoryInfo[] rootDirs, TreeNode parentNode)
+        {
+            TreeNode node;
+            foreach(DirectoryInfo dir in rootDirs)
+            {
+                try
+                {
+                    node = new TreeNode(dir.Name, (int)Icons.FolderClosed, (int)Icons.FolderOpen);
+                    node.Tag = dir;
+                    // Cogemos sólo el primer nivel de subdirectorios, no queremos recursión para no eternizar la carga
+                    if (dir.GetDirectories().Length > 0)
+                    {
+                        node.Nodes.Clear();
+                        foreach(DirectoryInfo sub in dir.GetDirectories())
+                        {
+                            TreeNode subNode = new TreeNode(sub.Name, (int)Icons.FolderClosed, (int)Icons.FolderOpen);
+                            subNode.Tag = sub;
+                            node.Nodes.Add(subNode);
+                        }
+                    }
+                    parentNode.Nodes.Add(node);
+                }
+                catch (UnauthorizedAccessException)
+                {
+                    // ignore error and keep at it
+                }
+            }
+        }
+
+        private void GetFiles(DirectoryInfo info)
+        {
+            lstFiles.Items.Clear();
+            ListViewItem.ListViewSubItem[] subItems;
+            ListViewItem item = null;
+
+            foreach (FileInfo file in info.GetFiles())
+            {
+                item = new ListViewItem(file.Name, (int)Icons.ImageFile);
+                lstFiles.Items.Add(item);
+            }
+
+            //lstFiles.AutoResizeColumns(ColumnHeaderAutoResizeStyle.HeaderSize);
+        }
+
+        private void GetFiles(DriveInfo info)
+        {
+            lstFiles.Items.Clear();
+            ListViewItem.ListViewSubItem[] subItems;
+            ListViewItem item = null;
+
+            foreach (FileInfo file in info.RootDirectory.GetFiles())
+            {
+                item = new ListViewItem(file.Name, 1);
+                subItems = new ListViewItem.ListViewSubItem[]
+                    { new ListViewItem.ListViewSubItem(item, "File"),
+             new ListViewItem.ListViewSubItem(item,
+                file.LastAccessTime.ToShortDateString())};
+
+                item.SubItems.AddRange(subItems);
+                lstFiles.Items.Add(item);
+            }
+
+            lstFiles.AutoResizeColumns(ColumnHeaderAutoResizeStyle.HeaderSize);
+        }
+
+        private void tvFolders_NodeMouseClick(object sender, TreeNodeMouseClickEventArgs e)
+        {
+            // ignora los clicks en los controles de expandir y contraer del treeview
+            TreeViewHitTestInfo hit = tvFolders.HitTest(e.Location);
+            if (hit.Location != TreeViewHitTestLocations.PlusMinus)
+            {
+                TreeNode selectedNode = e.Node;
+                selectedNode.Nodes.Clear();
+
+                if (selectedNode.Tag.GetType() == typeof(DriveInfo))
+                {
+                    DriveInfo info = (DriveInfo)selectedNode.Tag;
+                    GetFolders(info.RootDirectory.GetDirectories(), selectedNode);
+                    GetFiles(info);
+                }
+                else if (selectedNode.Tag.GetType() == typeof(DirectoryInfo))
+                {
+                    DirectoryInfo info = (DirectoryInfo)selectedNode.Tag;
+                    GetFolders(info.GetDirectories(), selectedNode);
+                    GetFiles(info);
+                }
+
+                selectedNode.Expand();
+            }
         }
     }
 }
